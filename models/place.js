b09bdd581f2bfaeb47bb94d91d61219b91e2530b
SarikAnwar01 <sarikanwar007@gmail.com>
2022-10-02 18:23:07 +0545

    converting address to human readable form and othe

--- models/place.js
+++ models/place.js
@@ -1,9 +1,9 @@
-class Place {
-    constructor(title, imageUri, address, location) {
+export class Place {
+    constructor(title, imageUri, location) {
         this.title = title;
         this.imageUri = imageUri;
-        this.address = address;
-        this.location = location;  // {lat:0.141232,lng:122.322}
+        this.address = location.address;
+        this.location = { lat: location.lat, lng: location.log };  // {lat:0.141232,lng:122.322}
         this.id = new Date().toString() + Math.random().toString();
     }
 }
